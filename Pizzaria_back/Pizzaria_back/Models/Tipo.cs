46ae2ab898bef6d3f479517fa64530a02b149ffe
Luiz Fernando Lima Avelar <fernandolfla@gmail.com>
2023-02-19 10:56:39 -0300

    Removido get set da propriedade do Objeto Produto produto em Tipo entity

--- Pizzaria_back/Pizzaria_back/Models/Tipo.cs
+++ Pizzaria_back/Pizzaria_back/Models/Tipo.cs
@@ -6,6 +6,6 @@ namespace Pizzaria_back.Models
     {
         public string Nome { get; set; } = string.Empty;
         public int ProdutoId { get; set;}
-        public Produto Produto { get; set;} //Quando deixo aqui ele reconhece que preciso passar um produto junto com o tipo
+        public Produto Produto; 
     }
 }
