a0fe9b2ed00c305e8ed7197cd4b098786c99c256
Fagner F Muniz dos Santos <fagner.santos@ciandt.com>
2023-03-10 09:22:04 -0300

    atualização login

--- Pizzaria_back/Pizzaria_back/Validators/UsuarioValidator.cs
+++ Pizzaria_back/Pizzaria_back/Validators/UsuarioValidator.cs
@@ -6,7 +6,7 @@ namespace Pizzaria_back.Validators
 {
     public class UsuarioValidator : AbstractValidator<Usuario>
     {
-        public UsuarioValidator(IUsuarioRepository usuarioRepository)
+        public UsuarioValidator(IUsuarioRepository usuarioRepository, bool eLogin)
         {
             RuleFor(x => x.Email).EmailAddress()
                                  .WithMessage("Email invalido");
@@ -14,8 +14,9 @@ namespace Pizzaria_back.Validators
             RuleFor(x => x.Senha).Matches("^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%¨&*()-+])[0-9a-zA-Z!@#$%¨&*()-+]{8,}$")
                                  .WithMessage("A senha deve conter ao menos um dígito, uma letra minúscula,uma letra maiúscula,um caractere especial e 8 dos caracteres");
 
-            RuleFor(x => x.Email).Must((email) => !usuarioRepository.Buscar().Any(x => x.Ativo && x.Email == email))
-                                 .WithMessage("Não é possivel cadastrar uma conta para este email.");
+            if (!eLogin)
+                RuleFor(x => x.Email).Must((email) => !usuarioRepository.Buscar().Any(x => x.Ativo && x.Email == email))
+                                     .WithMessage("Não é possivel cadastrar uma conta para este email.");
         }
     }
 }
